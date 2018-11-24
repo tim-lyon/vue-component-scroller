808a48e5363546bfb8035a036a01e0f930ff41e3
Tim Lyon <timlyon87@gmail.com>
2018-11-24 18:59:18 +0000

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="component">
+    My component!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'component-scroller',
+}
+</script>
+
+<style>
+#component{
+  overflow-y:scroll;
+  outline: none;
+  margin:0;
+  padding:0;
+}
+</style>
