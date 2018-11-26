494b0446d495e352ee107d96ccda48b92a701a58
Tim Lyon <npm@zippie.app>
2018-11-26 10:18:03 +0000

    Update to v0.2.0

--- README.md
+++ README.md
@@ -4,40 +4,54 @@ A virtual scroller for rendering (very) long lists of vue components.
 [Live demo](https://tim-lyon.github.io/vue-component-scroller/)
 
 ## Install
-`npm install vue-component-scroller`
+```
+npm install vue-component-scroller
+```
 
 ## Use
-The package exports a vue component which takes three props:
- - `item` is a component that you want to render in the list
- - `itemCount` is the number of components you want in the list
- - `itemHeight` is the fixed height (in pixles) that the components will be rendered at.
+The package exports a vue component which takes two props:
+ - `itemCount` is the number of items you want in the list
+ - `itemHeight` is the fixed height (in pixles) of each item
  
->Unfortunately, a fixed height for the `item` component is necessary - to pre-calculate the total height of millions of unique components would take too long.
+>Unfortunately, a fixed height for each `item` is necessary - to pre-calculate the total height of all the list items would take too long.
 
-The `item` component is passed a prop `index` for every instance in the list, allowing it to fetch data or render content based on its position in the list. This makes is particularly useful for rending long lists of repetitive data: table rows, contacts, photos etc *without* having to fetch any of the data in advance.
+Each item is given a slot for adding content. The variable `index` can be used inside the slot, allowing it to fetch data or render content based on its position in the list. This makes it particularly useful for rending long lists of repetitive data: table rows, contacts, photos etc *without* having to fetch any of the data in advance.
 
-In the code below, the `ComponentScroller` will render a (high performance) scrolling list of 100,000 instances of  `MyComponent`.
+In the code below (complete sinlge file component), the `VueComponentScroller` should render a (high performance) scrolling list of 100,000 items.
 
 ```html
 <template>
   <div id="app">
-    <component-scroller
-      :item="MyComponent"
-      itemCount="100000"
-      itemHeight="60"
-      style="height:400px;"/>
+    <vue-component-scroller class="scroll" :itemCount="100000" :itemHeight="50">
+      <div class="item" slot-scope="{index}">
+        Item number {{index}}
+      </div>
+    </vue-component-scroller>
   </div>
 </template>
 
 <script>
-import ComponentScroller from 'vue-component-scroller'
-import MyComponent from './components/MyComponent.vue'
-
+import VueComponentScroller from 'vue-component-scroller'
 export default {
-  components: {	ComponentScroller },
-  data() {
-    return { MyComponent } // MyComponent is in data so it
-  }                        // can be passed as a prop!
+  components: {
+    VueComponentScroller
+  }
 }
 </script>
+
+<style>
+.scroll {
+  height: 300px;
+  margin:50px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  border:2px solid red;
+}
+.item {
+  text-align: center;
+  height:40px;
+  margin:auto 20px;
+  box-sizing: border-box;
+  box-shadow: 0 3px 6px rgba(0,0,0,.16),0 3px 6px rgba(0,0,0,.23);
+}
+</style>
 ```
--- src/Component.vue
+++ src/Component.vue
@@ -1,83 +1,107 @@
 <template>
-  <div :style="{overflowY: 'scroll', outline: 'none'}" @scroll="onScroll" tabindex="1">
-    <div :style="{marginTop:topMargin+'px',marginBottom:bottomMargin+'px',}">	
-      <component v-for="itemIndex in visibleItems"
-		:style="{height:itemHeight+'px',overflow: 'hidden'}"
-        :is="item"
-        :index="itemIndex" 
-        :key="itemIndex"/>
-	</div>
+  <div :style="scrollerStyle" @scroll="onScroll" tabindex="1">
+    <div :style="containerStyle">	
+      <div v-for="i in visibleItems" :style="itemStyle" :index="i" :key="i">
+        <slot :index='i' />
+			</div>
+		</div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'ScrollingDiv',
+  name: "VueComponentScroller",
   props: {
     itemHeight: Number,
-	itemCount: Number,
-	item: Object,
-	bufferSize: {
+    itemCount: Number,
+    bufferSize: {
       type: Number,
       default: 5
-    },
+    }
   },
   data() {
-	return {
-		startItem: 0,
-		visibleItemCount: 0,
-		topMargin: 0,
-		bottomMargin: 0,
-		scrollTop: 0
-		}
+    return {
+      startItem: 0,
+      visibleItemCount: 0,
+      topMargin: 0,
+      bottomMargin: 0,
+      scrollTop: 0,
+      scrollerStyle: {
+        overflowY: "scroll",
+        outline: "none"
+      }
+    };
   },
-  mounted(){
-	this.initialise()
+  mounted() {
+    this.initialise();
   },
   watch: {
-	itemCount(){
-	  this.initialise()
-	},
-	startItem(){
-		this.topMargin = Math.max(0,Math.floor(this.itemHeight*this.startItem/this.marginFactor))
-		this.bottomMargin = this.totalMargin - this.topMargin
-	},
-	scrollTop(){
-		this.startItem = Math.ceil(this.scrollTop*this.marginFactor/this.itemHeight)-this.bufferSize
-		this.startItem = Math.min(this.startItem, this.maxStartItem)
-		this.startItem = Math.max(this.startItem, -this.bufferSize)
-	}
+    itemCount() {
+      this.initialise();
+    },
+    startItem() {
+      this.topMargin =
+        Math.max(
+          0,
+          Math.floor(
+            (this.itemHeight * (this.startItem - 1)) / this.marginFactor
+          )
+        ) +
+        this.itemHeight / 2;
+      this.bottomMargin = this.totalMargin - this.topMargin;
+    },
+    scrollTop() {
+      this.startItem =
+        Math.round((this.scrollTop * this.marginFactor) / this.itemHeight) -
+        this.bufferSize;
+      this.startItem = Math.min(this.startItem, this.maxStartItem);
+      this.startItem = Math.max(this.startItem, -this.bufferSize);
+    }
   },
   computed: {
-	visibleItems(){
-		let visibleItems = []
-		for(var i=0; i<=this.visibleItemCount; ++i){
-			if(this.startItem + i>0 && this.startItem + i<=this.itemCount){
-				visibleItems.push(this.startItem + i)
-			}
-		}
-		return visibleItems
-	}
+    visibleItems() {
+      let visibleItems = [];
+      for (var i = 0; i <= this.visibleItemCount; ++i) {
+        const item = this.startItem + i;
+        if (item > 0 && item <= this.itemCount) {
+          visibleItems.push(item);
+        }
+      }
+      return visibleItems;
+    },
+    containerStyle() {
+      return {
+        marginTop: this.topMargin + "px",
+        marginBottom: this.bottomMargin + "px"
+      };
+    },
+    itemStyle() {
+      return {
+        height: this.itemHeight + "px",
+        overflow: "hidden"
+      };
+    }
   },
   methods: {
-    initialise(){
-	  this.startItem = -this.bufferSize
-	  const containerHeight = this.$el.clientHeight
-	  this.visibleItemCount = Math.floor((containerHeight-2*this.itemHeight)/this.itemHeight)+2*this.bufferSize
-	  const visibleHeight = this.itemHeight*this.visibleItemCount
-	  const realTotalHeight = this.itemHeight*this.itemCount
-	  const totalHeight = Math.min(realTotalHeight,10e6)
-	  this.totalMargin = totalHeight - visibleHeight
-	  this.marginFactor = (realTotalHeight-visibleHeight)/(this.totalMargin)
-	  this.maxScrollTop = Math.max(0, totalHeight - containerHeight)
-	  this.maxStartItem = Math.max(0, this.itemCount - this.visibleItemCount)
-	  this.topMargin = 0
-	  this.bottomMargin = this.totalMargin
-	},
-	onScroll(){
-		this.scrollTop = this.$el.scrollTop
-	}
+    initialise() {
+      this.startItem = -this.bufferSize;
+      const containerHeight = this.$el.clientHeight;
+      this.visibleItemCount =
+        Math.floor((containerHeight - 2 * this.itemHeight) / this.itemHeight) +
+        2 * this.bufferSize;
+      const visibleHeight = this.itemHeight * this.visibleItemCount;
+      const realTotalHeight = this.itemHeight * this.itemCount;
+      const totalHeight = Math.min(realTotalHeight, 10e6);
+      this.totalMargin = totalHeight - visibleHeight;
+      this.marginFactor = (realTotalHeight - visibleHeight) / this.totalMargin;
+      this.maxScrollTop = Math.max(0, totalHeight - containerHeight);
+      this.maxStartItem = Math.max(0, this.itemCount - this.visibleItemCount);
+      this.topMargin = 0;
+      this.bottomMargin = this.totalMargin;
+    },
+    onScroll() {
+      this.scrollTop = this.$el.scrollTop;
+    }
   }
-}
+};
 </script>
